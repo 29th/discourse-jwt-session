e9ce7cf718cbd58f21285427b8ae40d7ed2a7ae8
wilson29thid <wilson@29th.org>
2022-01-27 21:36:27 +0000

    Add old cookie_hash method

--- plugin.rb
+++ plugin.rb
@@ -36,6 +36,24 @@ after_initialize do
       cookie_name = SiteSetting.jwt_session_cookie_name
       cookies.delete(cookie_name, domain: :all)
     end
+    
+    def cookie_hash(unhashed_auth_token)
+      hash = {
+        value: unhashed_auth_token,
+        httponly: true,
+        secure: SiteSetting.force_https
+      }
+  
+      if SiteSetting.persistent_sessions
+        hash[:expires] = SiteSetting.maximum_session_age.hours.from_now
+      end
+  
+      if SiteSetting.same_site_cookies != "Disabled"
+        hash[:same_site] = SiteSetting.same_site_cookies
+      end
+  
+      hash
+    end
   end
 
 
