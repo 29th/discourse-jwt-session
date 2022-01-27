01e207bb97fea62fb739ea3023355f2c6eb3af33
wilson29thid <wilson@29th.org>
2022-01-27 20:53:13 +0000

    Move class to plugin.rb and wrap in after_initialize

--- plugin.rb
+++ plugin.rb
@@ -10,6 +10,34 @@ enabled_site_setting :jwt_session_enabled
 
 PLUGIN_NAME ||= 'DiscourseJwtSession'
 
-load File.expand_path('lib/jwt_current_user_provider.rb', __dir__)
+after_initialize do
+  class JwtCurrentUserProvider < Auth::DefaultCurrentUserProvider
+    def log_on_user(user, session, cookies, opts = {})
+      super
 
-Discourse.current_user_provider = JwtCurrentUserProvider
+      cookie_name = SiteSetting.jwt_session_cookie_name
+      secret_key = GlobalSetting.safe_secret_key_base
+      algorithm = 'HS256'
+
+      payload = {
+        sub: user.id,
+        name: user.email
+      }
+
+      jwt = JWT.encode(payload, secret_key, algorithm)
+      jwt_cookie = cookie_hash(jwt)
+      jwt_cookie[:domain] = :all
+      cookies[cookie_name] = jwt_cookie
+    end
+
+    def log_off_user(session, cookies)
+      super
+
+      cookie_name = SiteSetting.jwt_session_cookie_name
+      cookies.delete(cookie_name, domain: :all)
+    end
+  end
+
+
+  Discourse.current_user_provider = JwtCurrentUserProvider
+end
